9500cbe5c17b4c81782d11cb176a2227c99a5962
Kei <opusk44@gmail.com>
2022-03-04 23:12:50 +0900

    add Header component to Home

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../component/Header'
 
 export default function Home() {
   return (
@@ -8,9 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Let's cloning hulu!!</h1>
-
-      {/* Header */}
+      <Header/>
 
       {/* NavBar */}
 
