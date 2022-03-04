e26110861dbcca447a92fcc400628e80c1f6120e
Kei <opusk44@gmail.com>
2022-03-04 23:06:36 +0900

    comment component layout

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,17 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Kulu</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <h1>Let's cloning hulu!!</h1>
+
+      {/* Header */}
+
+      {/* NavBar */}
+
+      {/* Results */}
     </div>
   )
 }
