a1031875c99e4bfc8c01244568a379ed7ca497b0
Kei <opusk44@gmail.com>
2022-03-04 23:24:32 +0900

    add logo & apply layer style

--- component/Header.js
+++ component/Header.js
@@ -1,7 +1,15 @@
+import Image from 'next/image';
+
 function Header() {
   return (
     <header>
       <h1>Let's cloning hulu!!</h1>
+
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200} height={100}
+      />
     </header>
   )
 }
