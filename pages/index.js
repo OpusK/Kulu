daf0b40c8d041488e8fba6ed86c1cf7515516055
Kei <opusk44@gmail.com>
2022-03-05 13:26:20 +0900

    Add Result component to Home & SSR impl.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -12,8 +14,25 @@ export default function Home() {
 
       <Header/>
       <Nav/>
-
-      {/* Results */}
+      <Results results={results} />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const { genre } = context.query;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  )
+  .then((res) => res.json())
+  .catch((error) => alert(error));
+
+  return {
+    props: {
+      results: request.results,
+    }
+  }
 }
